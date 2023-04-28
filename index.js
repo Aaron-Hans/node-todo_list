1d3980006953a6063e39bdfa055b6414ba44f459
Aaron-Hans <hansaaron@hotmail.fr>
2023-04-28 15:13:21 +0200

    final and first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const port = 3000;
+
+// const tasks = [
+//   {
+//     title: "Apprendre à programmer",
+//     done: false,
+//   },
+//   {
+//     title: "Faire les course",
+//     done: true,
+//   },
+// ];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  if (!req.session.tasks) {
+    req.session.tasks = [];
+  }
+  res.render("todolist", { tasks: req.session.tasks });
+});
+
+app.post("/task", (req, res) => {
+  if (req.body.task) {
+    req.session.tasks.push({
+      title: req.body.task,
+      done: false,
+    });
+  }
+  res.redirect("/");
+});
+
+app.get("/task/:id/done", (req, res) => {
+  if (req.session.tasks[req.params.id]) {
+    req.session.tasks[req.params.id].done = true;
+  }
+  res.redirect("/");
+});
+
+app.get("/task/:id/delete", (req, res) => {
+  if (req.session.tasks[req.params.id]) {
+    req.session.tasks.splice(req.params.id, 1);
+  }
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
